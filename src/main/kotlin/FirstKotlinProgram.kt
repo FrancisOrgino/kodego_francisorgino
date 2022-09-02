cbe10d850809f79d70b5d506b2a74df5554444e4
francis orgino <upworkfrancisorgino2889@gmail.com>
2022-09-02 19:35:12 +0800

    1st commit

---
+++ src/main/kotlin/FirstKotlinProgram.kt
@@ -0,0 +1,12 @@
+fun main(){
+
+    /*
+    Author: Francis
+    Date Created: 09/01/2022
+    Filename: FirstKotlinProgram.kt
+     */
+    print("Francis")
+    print("Orgino")
+    print("Hello World 4")
+
+}
