7531ea9d7bd584a83590b6809e151579127a502c
francis orgino <upworkfrancisorgino2889@gmail.com>
2022-09-03 12:36:25 +0800

    1st commit Activity01-B.kt

--- src/main/kotlin/exercises/Activity01-A.kt
+++ src/main/kotlin/exercises/Activity01-A.kt
@@ -1,2 +1,28 @@
+package exercises
+fun main() {
+
+    var student1 = "John Wick"
+    var student2 = "John Carter"
+    var student3 = "Dear John"
+
+    var grade1 = 80
+    var grade2 = 75
+    var grade3 = 90
+
+    var attendance1: Int = 95
+    var attendance2: Int = 90
+    var attendance3: Int = 80
+
+    println("name  $student1  grade  $grade1  attendance  $attendance1")
+
+    println("name  $student2  grade  $grade2  attendance  $attendance2")
+
+    println("name $student3  grade  $grade3  attendance  $attendance3")
 
 
+    var day1 = 1
+    var day2 = 1
+    var day3 = 1
+
+
+}
---
+++ src/main/kotlin/exercises/Activity01-B.kt
@@ -0,0 +1,15 @@
+package exercises
+fun main(){
+
+
+    var book1 = "Harry Potter"
+    var book2 = "Game of Thrones"
+    var book3 = "Lord of the Rings"
+
+    var status1 = "returned"
+    var status2 = "borrowed"
+
+    println("The book $book1 is $status1")
+    println("The book $book2 is $status1")
+    println("The book $book2 is $status2")
+}
