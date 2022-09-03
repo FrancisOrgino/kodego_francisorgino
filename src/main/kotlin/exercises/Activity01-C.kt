6e8c937e0c40687147e3bfe61bacc218d4d9697c
francis orgino <upworkfrancisorgino2889@gmail.com>
2022-09-03 13:03:49 +0800

    2nd commit Activity01-C.kt

---
+++ src/main/kotlin/exercises/Activity01-C.kt
@@ -0,0 +1,29 @@
+package exercises
+
+fun main (){
+
+    val category1 = "Instant Noodles"
+    val category2 = "Canned Meat"
+
+    var item1 = "Lucky me Beef"
+    var item2 = "Lucky me Chicken"
+    var item3 = "Lucky me Bulalo"
+
+    var item4 = "Highlands Cornedbeef"
+    var item5 = "Purefoods Cornedbeef"
+    var item6 = "Argentina Cornedbeef"
+
+    var stock1 = 100
+    var stock2 = 90
+    var stock3 = 80
+    var stock4 = 70
+    var sold1 = 10
+    var sold2 = 20
+    var sold3 = 30
+    var sold4 = 30
+
+
+
+
+
+}
