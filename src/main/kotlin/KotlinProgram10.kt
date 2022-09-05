6a22c6cd379fedda90bca122939b1fcde013f77b
francis orgino <upworkfrancisorgino2889@gmail.com>
2022-09-05 23:08:20 +0800

    1st commit

---
+++ src/main/kotlin/KotlinProgram10.kt
@@ -0,0 +1,16 @@
+fun main(){
+       var ctr1: Int = 1
+
+    while (ctr1 <= 5){
+        var ctr2 : Int = 1
+        while (ctr2 <= 2){
+            print("@")
+            ctr2++
+        }
+        println()
+        ctr1++
+
+    }
+
+
+}
