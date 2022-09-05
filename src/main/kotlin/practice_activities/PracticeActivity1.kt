9e249b73c5c93c10d24ae201f90af39b324019b2
francis orgino <upworkfrancisorgino2889@gmail.com>
2022-09-05 19:04:02 +0800

    1st commit

---
+++ src/main/kotlin/practice_activities/PracticeActivity1.kt
@@ -0,0 +1,23 @@
+package exercises
+
+fun main (){
+
+  println("Enter your  Firstname:")
+
+    var firstName :String = readln().toString()
+
+    println("My Firstname os $firstName")
+
+  println("Enter your middleName")
+
+  var middleName :String = readln().toString()
+
+  println("My Middle Name is $middleName")
+
+  println("Enter your  lastName")
+
+  var lastName : String = readln().toString()
+
+  println("My Last Name is $lastName")
+
+}
