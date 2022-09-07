c4db123f14135335f72051f4f291d47d3b014acd
francis orgino <upworkfrancisorgino2889@gmail.com>
2022-09-07 20:02:30 +0800

    2nd commit

---
+++ src/main/kotlin/KotlinProgram13.kt
@@ -0,0 +1,13 @@
+fun main(){
+
+    var names = arrayOf("john", "paul", "mark")
+    println(names[1])
+
+
+  var numbers = arrayOf<Int>(2,3,4,5,6,7,8,9,10)
+    println(numbers[5])
+
+    println(numbers.size)
+
+
+}
