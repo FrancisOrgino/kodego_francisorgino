63b4374bafcdeb344e332a17db60f246cf9cdddb
francis orgino <upworkfrancisorgino2889@gmail.com>
2022-09-10 15:21:11 +0800

    2nd commit

--- src/main/kotlin/practice_activities/PracticeActivity7.kt
+++ src/main/kotlin/practice_activities/PracticeActivity7.kt
@@ -1,16 +1,20 @@
 fun main(){
 
+
 palindromeChecker("String")
 }
 fun palindromeChecker(word:String) {
 
+    // first word input
     println("please type your word")
     var word: String = readln().toString()
     println("your word is $word please retype to check if palindrome")
 
+    // word to reverse
     var word2: String = readln().toString()
     var reverse = word.reversed()
 
+    // palindrome checker
     if (word2 == word) {
         println("your word $word2 is a palindrome")
     } else if (word2 == reverse) {
