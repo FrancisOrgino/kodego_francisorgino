218c7cf0cc484fffe7f2a3906b0853dce442107a
francis orgino <upworkfrancisorgino2889@gmail.com>
2022-09-18 00:25:04 +0800

    1st Commit

---
+++ src/main/kotlin/practice_activities/PracticeActivity5.kt
@@ -0,0 +1,22 @@
+fun main() {
+    // encoding first input
+    println("please type your number")
+    var number1 = arrayListOf<Int>(1,2,3,4,5)
+    var number2 = number1.reversed()
+
+    println("your numbers are $number1")
+    println("your reversed numbers are $number2")
+
+    // word to reverse
+
+
+
+}
+
+
+
+
+
+
+
+
