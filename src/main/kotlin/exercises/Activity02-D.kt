663e4896613bacb6adc47761b6af917c5aca4cff
francis orgino <upworkfrancisorgino2889@gmail.com>
2022-09-28 18:05:30 +0800

    3rd commit

--- src/main/kotlin/exercises/Activity02-D.kt
+++ src/main/kotlin/exercises/Activity02-D.kt
@@ -1,63 +1,76 @@
-fun main(){
+fun main() {
+    var studentInquiry = StudentInquiryFunction()
+    var status: Boolean = true
+    while(status) {
+        println()
+        println("Options")
+        println("[1] list of students")
+        println("[2] add students")
+        println("[3] encode grades")
+        println("[4] encode attendance")
+        println("[0] Exit")
+        var options: Int = readln().toInt()
 
-    var deposit1 = DepositAmount(0)
-         println("do you want to deposit?")
 
-    var response: String = readln().toString()
+        if (options == 1) {
+            studentInquiry.listOfStudents()
+        } else if (options == 2) {
+            println("enter student name")
+            var newStudent: String = readln()
+            studentInquiry.addNewStudent(newStudent)
+        } else if (options == 3) {
+            println("please enter student name")
+            var studentName: String = readln()
+            if (studentName in studentInquiry.studentList)
+                println("please enter student grade")
+            var encodedGrade: Int = readln().toInt()
 
-         if(response == "yes") {
-             depositor("yes",0)
-         }
-}
-    class DepositAmount(amount1: Int){
-
-        init {
-            println("welcome to the depository")
+        } else if (options == 4) {
+            var encodeAttendance = studentInquiry.addStudentAttendance()
+            encodeAttendance
+        } else if (options == 0) {
+            println("thank you")
+            status = false
         }
-
-}
-    fun depositor(response:String,amount1: Int):Int {
-        println("please enter the amount you want to deposit")
-        var amount1 = readln().toInt()
-
-    do {
-        println("do you want to continue?")
-
-        var response: String = readln().toString()
-
-
-        if (response == "yes") {
-            println("please add another amount")
-            var amount2: Int = readln().toInt()
-            amount1 = amount1 + amount2
-
-
-            // if no a set input is ask " do you want to add another amount"
-        } else if (response == "no") {
-            println("do you want to add another amount?")
-            response = readln()
-
-
-            // if quit total amount is showed and divisor is asked
-        }
-        if (response == "quit") {
-            println("your total amount is $amount1 divide by how many?")
-
-        }
-    } while (response == "yes")
-
-    // when divided total amount is will print
-
-    var divisor: Int = readln().toInt()
-    println("your denominations are ${amount1 / divisor}")
-        return amount1
+    }
 
 }
 
+class StudentInquiryFunction(){
+    var studentList = mutableListOf<String>()
+    var grade = mapOf<String,Int>()
+    var attendance = mapOf<String,Int>()
+
+
+    init {
+        println("welcome to student inquiry")
+    }
+
+    fun listOfStudents() {
+        println("these are the list of students")
+        for (student in studentList){
+            println(student)
+        }
+    }
+
+    fun addNewStudent(student: String) {
+        studentList.add(student)
+        println("$student is now added on the list")
+    }
+    fun encodeStudentGrade(studentName: String, encodeGrade:Int,){
 
 
 
 
 
+    }
 
+    fun addStudentAttendance() {
+        println("please enter student name")
+        var studentName: String = readln().toString()
+        println("please enter student attendance")
+        var addAttendance: Int = readln().toInt()
 
+    }
+
+}
--- src/main/kotlin/exercises/Activity02-F.kt
+++ src/main/kotlin/exercises/Activity02-F.kt
@@ -1,35 +1,263 @@
-fun main() {
-        println("please choose from the options")
-        println("[1} palindrome checker")
-        println("[0} exit")
+fun main(){
+    var buyGroceries = BuyGroceries()
+    var status: Boolean = true
+    var cupNoodles = arrayListOf<String>()
+    var cannedMeat = arrayListOf<String>()
 
-        var response: Int = readln().toInt()
+    cupNoodles.add("lucky me beef")
+    cupNoodles.add("lucky me chicken")
+    cupNoodles.add("lucky me bulalo")
 
-        if (response == 1) {
-            PalindromeChecker()
+    cannedMeat.add("delimondo")
+    cannedMeat.add("argentina")
+    cannedMeat.add("highlands")
+
+
+    while (status){
+
+            println("please select from the options")
+            println("[1]cup noodles list")
+            println("[2]canned meat list")
+            println("[3]buy cup noodles")
+            println("[4]buy canned meat")
+            println("[0]exit")
+
+        var options: Int = readln().toInt()
+        if (options == 1) {
+            println("these are the list of cup noodles")
+            println(cupNoodles)
+
         }
-        else if (response == 0)
+        else if (options == 2) {
+            println("these are the list of canned meat")
+            println(cannedMeat)
+        }
+        else if (options == 3) {
+            buyCupnoodles()
+        }
+
+        else if (options == 4) {
+            buyCannedMeat()
+        }
+        else if (options == 0) {
             println("thank you")
+            status = false
+        }
+
+    }
 
 }
 
-class palindrome(word:String){
 
-    init{
-        println("welcome to the palindrome checker")
+class BuyGroceries(){
+
+    init {
+        println("welcome to our grocery")
     }
 
 
-}fun PalindromeChecker(){
-    // encoding first input
-    println("please type your word")
-    var word: String = readln().toString()
-    var reversedWord = word.reversed()
+}
+fun buyCupnoodles3(){
 
-    if (word == reversedWord) {
-        println("your word is a palindrome")
-    }else {
-        println("your word is not a palindrome")
+    var qty: Int = 0
+    println("please enter noodles you want to buy")
+    var item1: String = readln().toString()
+
+    if(item1 == "lucky me beef"){
+        buyluckyMeBeef()
 
     }
+    else if(item1 =="lucky me chicken"){
+        buyluckyMeChicken()
+    }
+    else if(item1 =="lucky me bulalo"){
+        buyluckyMeBulalo()
+    }
+
+}
+fun buyCannedMeat3(){
+    var qty2: Int = 0
+    println("please enter canned meat you want to buy")
+    var item2: String = readln().toString()
+    if(item2 == "delimondo"){
+        buyDelimondo()
+    }
+    else if(item2 =="argentina"){
+        buyArgentina()
+    }
+    else if(item2 =="highlands"){
+        buyHighlands()
+    }
+
+} fun buyluckyMeBeef3() {
+
+    // response on item1 how many of this item
+    println("how many lucky me beef")
+    var item1: Int = readln().toInt()
+    var amount2: Int = 0
+    var stock1: Int = 100
+    //  response when asking if going to add
+    do {
+        println("do you want to add")
+        var response: String = readln().toString()
+
+
+        if (response == "yes") {
+            println("how many do you want to add")
+            amount2 = readln().toInt()
+            item1 = item1 + amount2
+
+            // if no it ends the line and total bought is printed and the remaining inventory shows
+        } else if (response == "no") {
+            println("the total you bought is $item1")
+            stock1 = stock1 - item1
+            println("current inventory is $stock1")
+
+
+        }
+    } while (response == "yes")
+}
+fun buyluckyMeChicken3(){
+
+    // response on item1 how many of this item
+    println("how many lucky me Chicken?")
+    var item1: Int = readln().toInt()
+    var amount2: Int = 0
+    var stock1: Int = 100
+
+    //  response when asking if going to add
+    do {
+        println("do you want to add")
+        var response: String = readln().toString()
+
+        if (response == "yes") {
+            println("how many do you want to add")
+            amount2= readln().toInt()
+            item1 = item1 + amount2
+
+            // if no it ends the line and total bought is printed and the remaining inventory shows
+        }
+        else if(response == "no"){
+            println("the total you bought is $item1 ")
+            var stock1: Int = 100
+            stock1 = stock1 - item1
+            println("current inventory is $stock1")
+
+        }
+    } while (response == "yes")
+
+}fun buyluckyMeBulalo3() {
+    // response on item1 how many of this item
+    println("how many lucky me bulalo")
+    var item1: Int = readln().toInt()
+    var amount2: Int = 0
+    var stock1: Int = 100
+
+    //  response when asking if going to add
+    do {
+        println("do you want to add")
+        var response: String = readln().toString()
+
+        if (response == "yes") {
+            println("how many do you want to add")
+            amount2= readln().toInt()
+            item1 = item1 + amount2
+
+            // if no it ends the line and total bought is printed and the remaining inventory shows
+
+        } else if (response == "no") {
+            println("the total you bought is $item1 ")
+            var stock1: Int = 100
+            stock1 = stock1 - item1
+            println("current inventory is $stock1")
+        }
+    } while (response == "yes")
+
+} fun buyDelimondo3() {
+
+    // response on item1 how many of this item
+    println("how many delimondo?")
+    var item1: Int = readln().toInt()
+    var amount2: Int = 0
+    var stock1: Int = 100
+    //  response when asking if going to add
+    do {
+        println("do you want to add")
+        var response: String = readln().toString()
+
+
+        if (response == "yes") {
+            println("how many do you want to add")
+            amount2 = readln().toInt()
+            item1 = item1 + amount2
+
+            // if no it ends the line and total bought is printed and the remaining inventory shows
+        } else if (response == "no") {
+            println("the total you bought is $item1")
+            stock1 = stock1 - item1
+            println("current inventory is $stock1")
+
+
+        }
+    } while (response == "yes")
+}
+fun buyArgentina3(){
+
+    // response on item1 how many of this item
+    println("how many argentina")
+    var item1: Int = readln().toInt()
+    var amount2: Int = 0
+    var stock1: Int = 100
+
+    //  response when asking if going to add
+    do {
+        println("do you want to add")
+        var response: String = readln().toString()
+
+
+        if (response == "yes") {
+            println("how many do you want to add")
+            amount2= readln().toInt()
+            item1 = item1 + amount2
+
+            // if no it ends the line and total bought is printed and the remaining inventory shows
+        }
+        else if(response == "no"){
+            println("the total you bought is $item1 ")
+            var stock1: Int = 100
+            stock1 = stock1 - item1
+            println("current inventory is $stock1")
+
+
+        }
+    } while (response == "yes")
+
+}fun buyHighlands3() {
+    // response on item1 how many of this item
+    println("how many lucky me highlands")
+    var item1: Int = readln().toInt()
+    var amount2: Int = 0
+    var stock1: Int = 100
+
+    //  response when asking if going to add
+    do {
+        println("do you want to add")
+        var response: String = readln().toString()
+
+
+        if (response == "yes") {
+            println("how many do you want to add")
+            amount2= readln().toInt()
+            item1 = item1 + amount2
+
+            // if no it ends the line and total bought is printed and the remaining inventory shows
+
+        } else if (response == "no") {
+            println("the total you bought is $item1 ")
+            var stock1: Int = 100
+            stock1 = stock1 - item1
+            println("current inventory is $stock1")
+        }
+    } while (response == "yes")
+
 }
