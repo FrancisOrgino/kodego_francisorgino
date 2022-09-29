1d3df9e56f68268352766d8800732be77f7be918
francis orgino <upworkfrancisorgino2889@gmail.com>
2022-09-29 21:49:18 +0800

    3rd commit

--- src/main/kotlin/exercises/Activity01-F.kt
+++ src/main/kotlin/exercises/Activity01-F.kt
@@ -1,23 +1,11 @@
 fun main(){
-    // encoding first input
-    println("please type your word")
-    var word: String = readln().toString()
-    println("your word is $word please retype to check if palindrome")
-
-    // word to reverse
-
-    var word2: String = readln().toString()
-    var reverse = word.reversed()
-
-
-    // palindrome checker
-    if(word2 == word) {
-        println("your word $word2 is a palindrome")
+    println("please enter your string")
+    var word: String = readln()
+    var word2 = word.reversed()
+    if(word == word2) {
+        println("your word $word is a palindrome")
     }
-
-        else if (word2 == reverse) {
+    else{
         println("your word $word is not a palindrome")
     }
-
-
 }
